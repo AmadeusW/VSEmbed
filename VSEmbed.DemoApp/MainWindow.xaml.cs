ff6d1d4511d49cf80978cc63161ee997ddd00aa6
Josh Varty <joshvarty@gmail.com>
2017-01-04 18:12:51 -0800

    Formatting

--- VSEmbed.DemoApp/MainWindow.xaml.cs
+++ VSEmbed.DemoApp/MainWindow.xaml.cs
@@ -13,13 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace VSEmbed.DemoApp {
+namespace VSEmbed.DemoApp
+{
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
+	public partial class MainWindow : Window
+	{
 
-		public MainWindow() {
+		public MainWindow()
+		{
 			InitializeComponent();
 		}
 
