5548a759cfbaffc246b3688984586830e92f331b
Josh Varty <joshvarty@gmail.com>
2017-01-04 16:31:01 -0800

    Include performance test project

---
+++ PerformanceTests/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using VSEmbed;
+
+namespace PerformanceTests
+{
+	class Program
+	{
+
+		private static void initializeRoslynForegroundThreadDataObject()
+		{
+			var currentThread = Thread.CurrentThread;
+			var assembly = Assembly.Load("Microsoft.CodeAnalysis.EditorFeatures");
+			var t_foregroundThreadData = assembly.GetType("Microsoft.CodeAnalysis.Editor.Shared.Utilities.ForegroundThreadData");
+			var m_createDefault = t_foregroundThreadData.GetMethod("CreateDefault", BindingFlags.Static | BindingFlags.NonPublic);
+			int foregroundThreadDataKind = 4;
+			var result = m_createDefault.Invoke(null, new object[] { foregroundThreadDataKind });
+			var t_foregroundThreadAffinitizedObject = assembly.GetType("Microsoft.CodeAnalysis.Editor.Shared.Utilities.ForegroundThreadAffinitizedObject");
+			var props = t_foregroundThreadAffinitizedObject.GetProperties().ToList();
+			var methods = t_foregroundThreadAffinitizedObject.GetMethods();
+			var m_currentForegroundThreadData = t_foregroundThreadAffinitizedObject.GetProperty("CurrentForegroundThreadData", BindingFlags.Static | BindingFlags.NonPublic);
+			m_currentForegroundThreadData.SetValue(null, result);
+		}
+
+		static void Main(string[] args)
+		{
+			var thread = new Thread(() =>
+			{
+				VsLoader.Load(new Version(14, 0, 0, 0));
+				VsServiceProvider.Initialize();
+				VsMefContainerBuilder.CreateDefault().Build();
+
+				//Can we please not have to do this?
+				initializeRoslynForegroundThreadDataObject();
+
+				var window = new VSEmbed.DemoApp.MainWindow();
+
+				new WpfApplication(window).Run();
+			});
+
+			thread.SetApartmentState(ApartmentState.STA);
+			thread.Start();
+			thread.Join();
+		}
+		private class WpfApplication : Application
+		{
+			private readonly Window _mainWindow;
+
+			public WpfApplication(Window mainWindow)
+			{
+				_mainWindow = mainWindow;
+			}
+
+			protected override void OnStartup(StartupEventArgs e)
+			{
+				_mainWindow.Show();
+			}
+		}
+
+	}
+
+
+}
