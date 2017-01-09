ae488e45b4d3830d0ff7315168bcb85c05cba1de
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-09 11:12:39 -0800

    removes designer code

--- VSEmbed/VsServiceProvider.cs
+++ VSEmbed/VsServiceProvider.cs
@@ -87,18 +87,6 @@ namespace VSEmbed
 
 			// Add services that use IServiceProvider here
 			sp.AddTaskSchedulerService();
-
-			// The designer loads Microsoft.VisualStudio.Shell.XX.0,
-			// which we cannot reference directly (to avoid breaking
-			// older versions). Therefore, I set the global property
-			// for every available version using Reflection instead.
-			foreach (var vsVersion in VsLoader.FindAllVersions().Where(v => v.Major >= 10)) {
-				var type = Type.GetType("Microsoft.VisualStudio.Shell.ServiceProvider, Microsoft.VisualStudio.Shell." + vsVersion.ToString(2));
-				if (type == null)
-					continue;
-				type.GetMethod("CreateFromSetSite", BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static)
-					.Invoke(null, new[] { sp });
-			}
 		}
 
 		private void AddTaskSchedulerService() {
