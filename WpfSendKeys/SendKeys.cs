4095382d417759ed1ae70d3d8df7c7ce5b2d23a6
Josh Varty <joshvarty@gmail.com>
2017-01-17 14:02:59 -0800

    All tabs.

--- WpfSendKeys/SendKeys.cs
+++ WpfSendKeys/SendKeys.cs
@@ -5,91 +5,91 @@ using System.Windows.Input;
 
 namespace WpfSendKeys
 {
-    public class SendKeys
-    {
-        public static void Send(UIElement element, string text)
-        {
-            var sequence = Parse(text);
-            foreach (var keyPressInfo in sequence)
-            {
-                Send(element, keyPressInfo);
-            }
-        }
+	public class SendKeys
+	{
+		public static void Send(UIElement element, string text)
+		{
+			var sequence = Parse(text);
+			foreach (var keyPressInfo in sequence)
+			{
+				Send(element, keyPressInfo);
+			}
+		}
 
-        public static void Send(UIElement element, KeyPressInfo keyPressInfo)
-        {
-            KeyboardDevice keyboardDevice = InputManager.Current.PrimaryKeyboardDevice;
-            if (keyPressInfo.Modifiers != ModifierKeys.None)
-            {
-                MockKeyboardDevice mockKeyboardDevice = MockKeyboardDevice.Instance;
-                mockKeyboardDevice.Modifiers = keyPressInfo.Modifiers;
-                keyboardDevice = mockKeyboardDevice;
-            }
-            RaiseKeyEvent(element, keyPressInfo, keyboardDevice);
-        }
+		public static void Send(UIElement element, KeyPressInfo keyPressInfo)
+		{
+			KeyboardDevice keyboardDevice = InputManager.Current.PrimaryKeyboardDevice;
+			if (keyPressInfo.Modifiers != ModifierKeys.None)
+			{
+				MockKeyboardDevice mockKeyboardDevice = MockKeyboardDevice.Instance;
+				mockKeyboardDevice.Modifiers = keyPressInfo.Modifiers;
+				keyboardDevice = mockKeyboardDevice;
+			}
+			RaiseKeyEvent(element, keyPressInfo, keyboardDevice);
+		}
 
-        public static void SendInput(UIElement element, string text)
-        {
-            InputManager inputManager = InputManager.Current;
-            InputDevice inputDevice = inputManager.PrimaryKeyboardDevice;
-            TextComposition composition = new TextComposition(inputManager, element, text);
-            TextCompositionEventArgs args = new TextCompositionEventArgs(inputDevice, composition);
-            args.RoutedEvent = UIElement.PreviewTextInputEvent;
-            element.RaiseEvent(args);
-            args.RoutedEvent = UIElement.TextInputEvent;
-            element.RaiseEvent(args);
-        }
+		public static void SendInput(UIElement element, string text)
+		{
+			InputManager inputManager = InputManager.Current;
+			InputDevice inputDevice = inputManager.PrimaryKeyboardDevice;
+			TextComposition composition = new TextComposition(inputManager, element, text);
+			TextCompositionEventArgs args = new TextCompositionEventArgs(inputDevice, composition);
+			args.RoutedEvent = UIElement.PreviewTextInputEvent;
+			element.RaiseEvent(args);
+			args.RoutedEvent = UIElement.TextInputEvent;
+			element.RaiseEvent(args);
+		}
 
-        private static void RaiseKeyEvent(UIElement element, KeyPressInfo keyPressInfo, KeyboardDevice keyboardDevice)
-        {
-            PresentationSource presentationSource = PresentationSource.FromVisual(element);
-            int timestamp = Environment.TickCount;
-            KeyEventArgs args = new KeyEventArgs(keyboardDevice, presentationSource, timestamp, keyPressInfo.Key);
+		private static void RaiseKeyEvent(UIElement element, KeyPressInfo keyPressInfo, KeyboardDevice keyboardDevice)
+		{
+			PresentationSource presentationSource = PresentationSource.FromVisual(element);
+			int timestamp = Environment.TickCount;
+			KeyEventArgs args = new KeyEventArgs(keyboardDevice, presentationSource, timestamp, keyPressInfo.Key);
 
-            // 1) PreviewKeyDown
-            args.RoutedEvent = Keyboard.PreviewKeyDownEvent;
-            element.RaiseEvent(args);
+			// 1) PreviewKeyDown
+			args.RoutedEvent = Keyboard.PreviewKeyDownEvent;
+			element.RaiseEvent(args);
 
-            // 2) KeyDown
-            args.RoutedEvent = Keyboard.KeyDownEvent;
-            element.RaiseEvent(args);
+			// 2) KeyDown
+			args.RoutedEvent = Keyboard.KeyDownEvent;
+			element.RaiseEvent(args);
 
-            // 3) TextInput
-            SendInputIfNecessary(element, keyPressInfo, keyboardDevice);
+			// 3) TextInput
+			SendInputIfNecessary(element, keyPressInfo, keyboardDevice);
 
-            // 4) PreviewKeyUp
-            args.RoutedEvent = Keyboard.PreviewKeyUpEvent;
-            element.RaiseEvent(args);
+			// 4) PreviewKeyUp
+			args.RoutedEvent = Keyboard.PreviewKeyUpEvent;
+			element.RaiseEvent(args);
 
-            // 5) KeyUp
-            args.RoutedEvent = Keyboard.KeyUpEvent;
-            element.RaiseEvent(args);
-        }
+			// 5) KeyUp
+			args.RoutedEvent = Keyboard.KeyUpEvent;
+			element.RaiseEvent(args);
+		}
 
-        private static void SendInputIfNecessary(UIElement element, KeyPressInfo keyPressInfo, KeyboardDevice keyboardDevice)
-        {
-            if (keyPressInfo.Modifiers.HasFlag(ModifierKeys.Control) || keyPressInfo.Modifiers.HasFlag(ModifierKeys.Alt))
-            {
-                return;
-            }
+		private static void SendInputIfNecessary(UIElement element, KeyPressInfo keyPressInfo, KeyboardDevice keyboardDevice)
+		{
+			if (keyPressInfo.Modifiers.HasFlag(ModifierKeys.Control) || keyPressInfo.Modifiers.HasFlag(ModifierKeys.Alt))
+			{
+				return;
+			}
 
-            string input = keyPressInfo.Input;
-            if (string.IsNullOrEmpty(input))
-            {
-                return;
-            }
+			string input = keyPressInfo.Input;
+			if (string.IsNullOrEmpty(input))
+			{
+				return;
+			}
 
-            if (keyPressInfo.Modifiers == ModifierKeys.Shift)
-            {
-                input = input.ToUpperInvariant();
-            }
-            else
-            {
-                input = input.ToLowerInvariant();
-            }
+			if (keyPressInfo.Modifiers == ModifierKeys.Shift)
+			{
+				input = input.ToUpperInvariant();
+			}
+			else
+			{
+				input = input.ToLowerInvariant();
+			}
 
-            SendInput(element, input);
-        }
+			SendInput(element, input);
+		}
 
 		private static IEnumerable<KeyPressInfo> Parse(string text)
 		{
