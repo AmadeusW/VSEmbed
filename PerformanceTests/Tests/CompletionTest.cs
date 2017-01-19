eeaea3c0cea51e195b8f187a19b88cb8404581b7
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-18 18:20:05 -0800

    completion tests

--- PerformanceTests/Tests/CompletionTest.cs
+++ PerformanceTests/Tests/CompletionTest.cs
@@ -6,8 +6,11 @@ namespace PerformanceTests.Tests
 {
 	public class CompletionTest : TestBase
 	{
-		[Params(0, 10)]
+		[Params(10, 100)]
 		public int IntellisenseLaunchCount { get; set; }
+
+		[Params(true, false)]
+		public bool LargeFile { get; set; }
 
 		[Params(Location.OutsideNamespace, Location.WithinClass, Location.WithinMethod)]
 		public Location CompletionLocation { get; set; }
@@ -16,18 +19,30 @@ namespace PerformanceTests.Tests
 		{
 			base.SetupHost();
 
-			Host.SetText(Snippets.ConsoleApp);
+			Host.SetText(Snippets.ConsoleApp + (LargeFile ? Snippets.ExtraCode : String.Empty));
 			Host.MoveCaret(Snippets.GetCaretPositionInConsoleApp(CompletionLocation));
 		}
 
-		[Benchmark, STAThread]
+		[Benchmark(Baseline = true, OperationsPerInvoke = 1), STAThread]
 		public void LaunchIntellisense()
 		{
+			Host.SendKey(System.Windows.Input.Key.Space);
 			// Test: Launch intellisense
 			for (int i = 0; i < IntellisenseLaunchCount; i++)
 			{
 				Host.SendKey(System.Windows.Input.Key.Space, System.Windows.Input.ModifierKeys.Control);
 				Host.SendKey(System.Windows.Input.Key.Escape); // Dismiss intellisense
+			}
+		}
+
+		[Benchmark(OperationsPerInvoke = 1), STAThread]
+		public void LaunchIntellisenseAndComplete()
+		{
+			Host.SendKey(System.Windows.Input.Key.S);
+			for (int i = 0; i < IntellisenseLaunchCount; i++)
+			{
+				Host.SendKey(System.Windows.Input.Key.Space, System.Windows.Input.ModifierKeys.Control);
+				Host.SendKey(System.Windows.Input.Key.Enter); // Complete
 			}
 		}
 	}
